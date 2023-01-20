24618901860e988b20c6fa59481fb8330263582f
Griffin Gradke <griffin.gradke@gmail.com>
2023-01-20 13:42:58 -0600

    Meals Section data render basic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react'
 
 import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
 
 const App = () => {
   return (
     <Fragment>
-        <Header></Header>
+        <Header />
+        <Meals />
     </Fragment>
   )
 }
